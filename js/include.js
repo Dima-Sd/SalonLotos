76d7a72cc3f7ee5991d236a4ba662bc9f063163b
Dima121111 <157640077+Dima121111@users.noreply.github.com>
2025-11-15 15:41:28 +0100

    added the website

---
+++ js/include.js
@@ -0,0 +1,49 @@
+// --- RESET ---
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = '/css/reset.css'; // <-- абсолютний шлях від кореня
+document.head.appendChild(link);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const path = window.location.pathname;
+
+  const isThankYouPage = path.includes('dziekujemy');
+  const isMasazePage = path.includes('/masaze/');
+
+  if (!isThankYouPage || isMasazePage) {
+    // --- HEADER ---
+    fetch("/header.html")
+      .then(response => response.text())
+      .then(data => {
+        document.body.insertAdjacentHTML("afterbegin", data);
+
+        const content = document.querySelector('.header');
+        if (content) {
+          window.addEventListener('scroll', () => {
+            const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+            content.classList.toggle('scrolled', scrollPosition >= 100);
+          });
+        }
+
+        const mobileButton = document.querySelector('.mobile-button');
+        const menu = document.querySelector('.menu');
+        const body = document.querySelector('body');
+
+        if (mobileButton && menu && body) {
+          mobileButton.addEventListener("click", function () {
+            const isHidden = menu.classList.contains('is-hidden');
+            this.classList.toggle('is-open', isHidden);
+            menu.classList.toggle('is-hidden', !isHidden);
+            body.classList.toggle('no-scroll', isHidden);
+          });
+        }
+      });
+
+    // --- FOOTER ---
+    fetch("/footer.html")
+      .then(response => response.text())
+      .then(data => {
+        document.body.insertAdjacentHTML("beforeend", data);
+      });
+  }
+});
